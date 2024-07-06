b95681a178e1729d6545b919a9b299cca0d1c776
kseniaagapitova <kagapitova@inbox.ru>
2024-07-06 11:24:23 +0300

    Test commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    'prettier/prettier': 'error',
+  },
+};
