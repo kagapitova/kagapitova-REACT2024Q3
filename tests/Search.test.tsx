6f4769331242b9f64c885003b8168206ede64f96
kseniaagapitova <kagapitova@inbox.ru>
2024-07-17 22:51:25 +0300

    add: tests for search click and LS

--- tests/Search.test.tsx
+++ tests/Search.test.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { vi, describe, it, expect, beforeEach } from 'vitest';
 import Search from '../src/Search';
 import '@testing-library/jest-dom';
-import { vi, expect } from 'vitest';
 
-type SearchServiceType = (searchTerm: string) => Promise<void>;
-const mockSearchService: SearchServiceType = vi.fn();
+describe('Search component', () => {
+  const mockSearchService = vi.fn(() => Promise.resolve());
 
-test('Search component renders correctly', () => {
-  const { getByText, getByPlaceholderText } = render(
-    <Search onSearch={mockSearchService} />,
-  );
-  const inputElement = getByPlaceholderText('Search...');
-  const buttonElement = getByText('Search');
-  expect(inputElement).toBeInTheDocument();
-  expect(buttonElement).toBeInTheDocument();
+  beforeEach(() => {
+    localStorage.clear();
+    mockSearchService.mockClear();
+  });
+
+  it('renders correctly', () => {
+    const { getByText, getByPlaceholderText } = render(
+      <Search onSearch={mockSearchService} />,
+    );
+    const inputElement = getByPlaceholderText('Search...');
+    const buttonElement = getByText('Search');
+    expect(inputElement).toBeInTheDocument();
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('saves the entered value to local storage when Search button is clicked', () => {
+    const { getByPlaceholderText, getByText } = render(
+      <Search onSearch={mockSearchService} />,
+    );
+    const inputElement = getByPlaceholderText('Search...');
+    const buttonElement = getByText('Search');
+
+    fireEvent.change(inputElement, { target: { value: 'test search' } });
+    expect(inputElement).toHaveValue('test search');
+
+    fireEvent.click(buttonElement);
+
+    expect(localStorage.getItem('searchTerm')).toBe('test search');
+  });
+
+  it('calls onSearch with the trimmed search term when Search button is clicked', () => {
+    const { getByPlaceholderText, getByText } = render(
+      <Search onSearch={mockSearchService} />,
+    );
+    const inputElement = getByPlaceholderText('Search...');
+    const buttonElement = getByText('Search');
+
+    fireEvent.change(inputElement, { target: { value: '   test search   ' } });
+    expect(inputElement).toHaveValue('   test search   ');
+
+    fireEvent.click(buttonElement);
+
+    expect(mockSearchService).toHaveBeenCalledWith('test search');
+  });
 });
